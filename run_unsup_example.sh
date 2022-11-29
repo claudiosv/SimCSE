382c15e517864f9eb5f5ec2acb5a45ef09000a7e
claudiosv <claudiosv@users.noreply.github.com>
2022-11-29 11:37:48 -0800

    Disable eval (we don't have an evaluator for trace embeddings yet)

--- run_unsup_example.sh
+++ run_unsup_example.sh
@@ -13,16 +13,14 @@ python train.py \
 	--per_device_train_batch_size 64 \
 	--learning_rate 3e-5 \
 	--max_seq_length 32 \
-	--evaluation_strategy steps \
+	--evaluation_strategy no \
 	--metric_for_best_model stsb_spearman \
 	--load_best_model_at_end \
-	--eval_steps 125 \
 	--pooler_type cls \
 	--mlp_only_train \
 	--overwrite_output_dir \
 	--temp 0.05 \
 	--do_train \
-	--do_eval \
 	--do_mlm False \
 	--fp16 \
 	"$@"
