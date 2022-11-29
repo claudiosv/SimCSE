9e454c22ea8a8a9db26771cac0e0c9bbb6c95a3f
Claudio Spiess <claudiosv@noreply.github.com>
2022-11-29 19:13:56 +0000

    try tracebert

--- run_unsup_example.sh
+++ run_unsup_example.sh
@@ -3,9 +3,10 @@
 # In this example, we show how to train SimCSE on unsupervised Wikipedia data.
 # If you want to train it with multiple GPU cards, see "run_sup_example.sh"
 # about how to use PyTorch's distributed data parallel.
+#princeton-nlp/sup-simcse-bert-base-uncased \
 
 python train.py \
-	--model_name_or_path princeton-nlp/sup-simcse-bert-base-uncased \
+	--model_name_or_path /home/claudios/data/traces/notebooks_OLD/tracebert_019/checkpoint-84000 \
 	--train_file data/wiki1m_for_simcse.txt \
 	--output_dir result/my-unsup-simcse-bert-base-uncased \
 	--num_train_epochs 1 \
